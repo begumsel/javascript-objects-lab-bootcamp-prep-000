eba47ad78d5d1227f12939d4b51e823c14521105
Begum S. Tuyen <begumtuyen@gmail.com>
2019-01-17 20:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
   var recipes = {}
 
   function updateObjectWithKeyAndValue(object, key, value){
@@ -13,6 +14,25 @@
   }
   function destructivelyDeleteFromObjectByKey(object, key){
     return delete object[key]
+=======
+function Objects(){
+  recipes = {prop: 1}
+}
+  
+  function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, recipes, { prop2: ['2'] })
+}
+
+  function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+    return recipes.prop2='2'
+  }
+  function deleteFromObjectByKey(object, key){
+    var newObj = Oject.assign({}, recipes)
+    return delete newObj.prop2
+  }
+  function destructivelyDeleteFromObjectByKey(object, key){
+  return recipes.prop
+>>>>>>> e9197d1072ca3fe77a2d144645d0d1f28dd0ea79
 }
 
 
